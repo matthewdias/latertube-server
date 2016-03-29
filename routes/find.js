fbf150eb68fd200ac6b1effb606968a9ed6de8b2
matthewdias <matthewdias@me.com>
2016-03-29 02:10:22 -0500

    support pages with multiple embeds

--- routes/find.js
+++ routes/find.js
@@ -6,52 +6,66 @@ var Horseman = require('node-horseman');
 router.get(/.+\..+/, function(req, res, next) {
 	url = req.originalUrl.substr(6);
 	console.log(url);
-	json = {}
+	json = [];
+	j = {};
 	if(url.includes('youtube.com')) {
 		base = url.match(/youtube.com\/([^\?]+)/)[0];
 		kind = base.split('/')[1];
 		if(kind == 'watch') {
-			json.kind = 'video';
-			json.id = url.match(/v=([^&]+)/)[0].substr(2);
+			j.kind = 'video';
+			j.id = url.match(/v=([^&]+)/)[0].substr(2);
 		}
 		else if(kind == 'playlist') {
-			json.kind = 'playlist';
-			json.id = url.match(/list=([^&]+)/)[0].substr(5);
+			j.kind = 'playlist';
+			j.id = url.match(/list=([^&]+)/)[0].substr(5);
 		}
-		json.url = url;
+		j.url = url;
+		json.push(j);
 		res.json(json);
 	}
 	else if(url.includes('youtu.be')) {
-		json.kind = 'video';
+		j.kind = 'video';
 		tail = url.match(/youtu.be\/.+/)[0].substr(9);
-		json.id = tail.match(/[^&]+/)[0];
-		json.url = 'https://www.youtube.com/watch?v=' + tail;
+		j.id = tail.match(/[^&]+/)[0];
+		j.url = 'https://www.youtube.com/watch?v=' + tail;
+		json.push(j);
 		res.json(json);
 	}
 	else {
 		var horseman = new Horseman();
-		embeds = horseman
+		horseman
 			.open(url)
 			.evaluate(function(selector) {
-				embeds = $("iframe[src*='youtube']").attr("src");
-  				return embeds;
+				em = [];
+				$("iframe[src*='youtube']").each(function() {
+					em.push(this.getAttribute("src"));
+				});
+  				return em;
 			})
 			.then(function(embeds){
-    			console.log('embeds ' + embeds);
-    			split = embeds.match(/embed\/(.+)/)[0].split('/');
-    			embed = split[1];
-    			if(embed.includes('videoseries')) {
-    				json.kind = 'playlist';
-    				json.id = embed.match(/list=([^&]+)/)[0].substr(5);
-    				json.url = "https://www.youtube.com/playlist?list=" + json.id;
+				if(embeds) {
+    				console.log('embeds ' + embeds);
+    				for (embed in embeds) {
+    					console.log('embed ' + embeds[embed]);
+    					j = {};
+    					split = embeds[embed].match(/embed\/(.+)/)[0].split('/');
+    					e = split[1];
+    					if(e.includes('videoseries')) {
+    						j.kind = 'playlist';
+    						j.id = e.match(/list=([^&]+)/)[0].substr(5);
+    						j.url = "https://www.youtube.com/playlist?list=" + j.id;
+    					}
+    					else {
+    						j.kind = 'video';
+    						j.id = e.split('?')[0];
+    						j.url = "https://www.youtube.com/watch?v=" + e;
+    					}
+    					json.push(j);
+    				}
+    				res.json(json);
     			}
-    			else {
-    				json.kind = 'video';
-    				json.id = embed.split('?')[0];
-    				json.url = "https://www.youtube.com/watch?v=" + embed;
-    			}
-    			res.json(json);
-    			return horseman.close();
+    			else res.sendStatus(400);
+    			horseman.close();
     		});
 	}
 });
